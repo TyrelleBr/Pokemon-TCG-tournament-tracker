9f76cb48b19a8c2b29e0d6b2dfb59a3a7bab9253
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-17 06:55:07 -0800

    Create array to store error messages and if a field is empty append string to the end of errors array

--- phpStuff/formHandling.php
+++ phpStuff/formHandling.php
@@ -7,4 +7,19 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
     $password = trim($_POST['password']);
     $email = trim($_POST['email']);
     $dateOfBirth = trim($_POST['dateOfBirth']); 
+
+    $errors = [];
+
+    if (empty($username)) {
+        $errors[] = "Username is required";
+    }
+    if (empty($password)) {
+        $errors[] = "Password is required";
+    }
+    if (empty($email)) {
+        $errors[] = "Emaiil is required";
+    }
+    if (empty($dateOfBirth)) {
+        $errors[] = "Your date of birth is required";
+    }
 }
