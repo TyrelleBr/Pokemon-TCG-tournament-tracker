9323bdab4b70f984cdbc9cfa00079f0ee0f55aef
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-17 14:04:09 -0800

    Make if statement that requires user to enter both username and password

--- handlers/login.php
+++ handlers/login.php
@@ -7,5 +7,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
 
+    if (empty($username) || empty($password)) {
+        echo "Please and username AND password";
+        exit;
+    }
+
+    
 
 }
