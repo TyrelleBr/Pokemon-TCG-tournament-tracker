d5e94fd33cb5c49cdbd523c5fb2aacc941a51417
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-17 13:36:07 -0800

    Create variables for database connection to make it more dynamic if needed

--- phpStuff/config.php
+++ phpStuff/config.php
@@ -1,6 +1,11 @@
 <?php
 
-$dbConnection = new mysqli('localhost', "root", "", "pokemon_tcg");
+$host = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'pokemon_tcg';
+
+$dbConnection = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 
 if ($dbConnection->connect_error) {
     die("Database connection failed:" . $dbConnection->connect_error);
