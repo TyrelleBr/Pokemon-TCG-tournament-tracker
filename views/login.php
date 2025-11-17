c3eb284fdaef73cd29128a93d0c10cdd662b462c
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-17 15:13:12 -0800

    Fix html error causing login form to not work

--- views/login.php
+++ views/login.php
@@ -16,7 +16,7 @@ include '../partials/head.php';
           <label class="sign-up-label" for="username">Username:</label><br />
           <input type="text" id="username" name="username" /><br />
           <label for="password">Password:</label><br />
-          <input type="text" id="password" name="password" /><br />
+          <input type="password" id="password" name="password" /><br />
           <input class="btn" type="submit" value="Submit" />
         </form>
 
