c0e5d4654f8b43e0fd05ed19f43f6acfe55bea70
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-17 14:02:34 -0800

    Take inputs and trim the whitespace

--- handlers/login.php
+++ handlers/login.php
@@ -1,3 +1,11 @@
 <?php 
 
 include '../handlers/config.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+
+}
