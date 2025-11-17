9b16dc3afe58f3b0021b2040fed08ceb35c27261
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-17 15:35:12 -0800

    Fix small syntax error causing error code

--- partials/navigation.php
+++ partials/navigation.php
@@ -13,7 +13,7 @@
 
                 if (isset($_SESSION['user_id'])) {
                     echo '<a class="btn" href="profile.php">Profile</a>';
-                    echo '<a href="handlers/logout.php">Logout</a>'
+                    echo '<a href="handlers/logout.php">Logout</a>';
                 } else {
                     echo '<a class="btn" href="login.php">Log-in/Sign-up</a>';
                 }
