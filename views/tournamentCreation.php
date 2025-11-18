a3a79d36f910998836664f48da47413ba0acdd55
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-17 17:57:23 -0800

    Remove label names and replace with H2's

--- views/tournamentCreation.php
+++ views/tournamentCreation.php
@@ -8,25 +8,31 @@ include '../partials/navigation.php';
 
 ?>
 
-<form action="createTournament.php" method="POST">
-    <h2>Tournament Details</h2>
-    <label for="tournamentName">Tournament Name</label>
+<div class="create-tournament-card">
+
+<form class="createTForm" action="createTournament.php" method="POST">
+    <h1>Tournament Details</h1>
+    <h2>Tournament Name</h2>
+    <label for="tournamentName"></label>
     <input type="text" id="tournamentName" name="tournamentName" required>
-    <label for="description">Description</label>
+    <h2>Description</h2>
+    <label for="description"></label>
     <textarea id="description" name="description" rows="4" required></textarea>
     <h2>Rules</h2>
-    <label for="rules">Tournament Rules</label>
+    <label for="rules"></label>
     <textarea id="rules" name="rules" rows="4" required></textarea>
     <h2>Start Date</h2>
-    <label for="startDate">Start Date</label>
+    <label for="startDate"></label>
     <input type="date" id="startDate" name="startDate" required>
     <h2>Location</h2>
-    <label for="location">Location</label>
+    <label for="location"></label>
     <input type="text" id="location" name="location" required>
     <h2>Contact Email</h2>
-    <label for="contactEmail">Email</label>
+    <label for="contactEmail"></label>
     <input type="email" id="contactEmail" name="contactEmail" required>
-    <button type="submit">Create Tournament</button>
+    <button class="btn" type="submit">Create Tournament</button>
 </form>
+
+</div>
 
 <?php include '../partials/footer.php'; ?>
