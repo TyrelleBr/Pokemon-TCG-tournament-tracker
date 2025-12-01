ec0ce33a8fee2f7eb84bc0b762e9b3f0bc29facc
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-30 22:18:59 -0800

    Use a prepare statement to query the database for tournament row with matching id

--- handlers/deleteTournament.php
+++ handlers/deleteTournament.php
@@ -6,3 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     $id = (int)$_POST['id'];
 
+    $statement = $dbConnection->prepare("SELECT userId FROM tournaments WHERE id = ?");
+    $statement->bind_param("i", $id);
+    $statement->execute();
+    $result = $statement->get_result();
+    $tournament = $result->fetch_assoc();
+    $statement->close();
