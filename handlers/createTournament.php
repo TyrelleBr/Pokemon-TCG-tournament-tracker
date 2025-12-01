8e89f47e4f1a332d15184e0c993577105be98071
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-30 18:07:59 -0800

    If the user is not logged in when creating a tournament send them to the login page

--- handlers/createTournament.php
+++ handlers/createTournament.php
@@ -4,12 +4,18 @@
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
+    if (!isset($_SESSION['user_id'])) {
+        header("location: ../views/login.php");
+        exit;
+    }
+
     $tournamentName = trim($_POST['tournamentName']);
     $description = trim($_POST['description']);
     $rules = trim($_POST['rules']);
     $startDate = trim($_POST['startDate']);
     $location = trim($_POST['location']);
     $contactEmail = trim($_POST['contactEmail']);
+    $userId = $_SESSION['user_id'];
 
 
     $statement = $dbConnection->prepare("INSERT INTO tournaments (tournamentName, description, rules, startDate, location, contactEmail) VALUES (?, ?, ?, ?, ?, ?)");
