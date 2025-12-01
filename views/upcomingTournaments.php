b61fca4a71a8e63f5cc97a67d259d8dd5d734028
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-30 18:26:22 -0800

    Query the database for certain columns then return the rows by start date to result

--- views/upcomingTournaments.php
+++ views/upcomingTournaments.php
@@ -11,6 +11,11 @@ include '../partials/navigation.php';
 <h1>Upcoming Tournaments</h1>
 
 
+<?php
 
+$result = $dbConnection->query("SELECT id, tournamentName, description, startDate FROM tournaments ORDER BY startDate ASC");
+
+
+?>
 
 <?php include '../partials/footer.php'; ?>
