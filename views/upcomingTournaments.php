8a576f07e47db12715f850db15c0667d74f5bd19
TyrelleBr <144159994+TyrelleBr@users.noreply.github.com>
2025-11-30 19:50:27 -0800

    Use loop to print a tournament card for each row using the row keys to input content and style in css

--- views/upcomingTournaments.php
+++ views/upcomingTournaments.php
@@ -6,16 +6,28 @@ include '../partials/head.php';
 
 include '../partials/navigation.php';
 
+include '../handlers/config.php'
+
 ?>
 
 <h1>Upcoming Tournaments</h1>
 
-
+<div class="upcoming-tournaments">
 <?php
 
 $result = $dbConnection->query("SELECT id, tournamentName, description, startDate FROM tournaments ORDER BY startDate ASC");
 
+while ($row = $result->fetch_assoc()) {
+    echo '<div class="tournament-card">';
+    echo '<h2>' . $row['tournamentName'] . '</h2>';
+    echo '<h3>Description:</h3>';
+    echo '<p>' . $row['description'] . '</p>';
+    echo '<a href="tournamentDetails.php?id=' . $row['id'] . '">View More</a>';
+    echo '</div>';
+}
 
 ?>
+
+</div>
 
 <?php include '../partials/footer.php'; ?>
